26754e9163505a9221d707e0f323d09e9f9f03f4
Go-Jaecheol <gojaech@naver.com>
2021-02-02 08:17:26 +0900

    Jfe solved BOJ_9251

--- BOJ/[9251] LCS/Jfe/BOJ_9251.py
+++ BOJ/[9251] LCS/Jfe/BOJ_9251.py
@@ -1,14 +1,14 @@
 A = input()
 B = input()
-dp = [[0 for x in range(len(B))]for y in range(len(A))]
+dp = [[0 for x in range(len(B)+1)]for y in range(len(A)+1)]
 
 for i in range(len(A)):
+    row = i + 1
     for j in range(len(B)):
+        col = j + 1
         if(A[i] == B[j]):
-            if (A[i] != A[i-1]):
-                dp[i][j] = dp[i-1][j] + 1
-            else:
-                dp[i][j] = dp[i-1][j-1] + 1
-        dp[i][j] = max(dp[i][j-1], dp[i][j], dp[i-1][j])
+            dp[row][col] = dp[row-1][col-1] + 1
+        else:
+            dp[row][col] = max(dp[row][col-1], dp[row-1][col])
         
-print(max(dp[len(A)-1]))
+print(max(dp[len(A)]))
