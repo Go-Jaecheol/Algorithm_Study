4a4f32e988320f59109be5d730706a261c997528
e70e6916 <dgkim69166916@gmail.com>
2021-03-06 18:49:29 +0900

    update BOJ_16234.py and README.md

--- BOJ/[16234] 인구 이동/e70e6916/BOJ_16234.py
+++ BOJ/[16234] 인구 이동/e70e6916/BOJ_16234.py
@@ -4,21 +4,15 @@ land = list(list(map(int, input().split())) for _ in range(N))
 direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # 상, 하, 좌, 우
 
 
-def change_population():
-    for i, v in enumerate(union):
-        for x, y in v:
-            land[x][y] = union_avg[i]
-
-
 def move_population(start_x, start_y):
     global check
     visited[start_x][start_y] = True
     sum_union = land[start_x][start_y]
-    temp = []
     queue = deque([(start_x, start_y)])
+
     while queue:
         i, j = queue.popleft()
-        temp.append((i, j))
+        union.append((i, j))
         for d in direction:
             x, y = i + d[0], j + d[1]
             if 0 <= x < N and 0 <= y < N and not visited[x][y]:
@@ -27,24 +21,23 @@ def move_population(start_x, start_y):
                     sum_union += land[x][y]
                     queue.append((x, y))
 
-    if len(temp) > 1:  # 이동이 발생
+    if len(union) > 1:  # 인구이동이 일어남
         check = True
-        union.append(temp)
-        union_avg.append(sum_union // len(temp))
+        for x, y in union:
+            land[x][y] = sum_union // len(union)
 
 
 check = True
 move_cnt = 0
 while check:
     check = False
-    union, union_avg = [], []
     visited = list([False] * N for _ in range(N))
     for i in range(N):
         for j in range(N):
             if not visited[i][j]:
+                union = []
                 move_population(i, j)
     if check:
-        change_population()
         move_cnt += 1
 
 print(move_cnt)
