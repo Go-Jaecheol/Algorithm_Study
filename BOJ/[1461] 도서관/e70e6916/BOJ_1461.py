4cc7a6baa964efca4e8762f545b1ef0ba12e154e
e70e6916 <dgkim69166916@gmail.com>
2021-03-08 18:49:56 +0900

    create BOJ_1461.py

---
+++ BOJ/[1461] 도서관/e70e6916/BOJ_1461.py
@@ -0,0 +1,26 @@
+N, M = map(int, input().split())
+lib = list(list(map(int, input().split())))
+
+abs_lib, sum_step = [], 0
+for i, step in enumerate(lib):
+    abs_lib.append([abs(step), True])
+    if step < 0:
+        abs_lib[i][1] = False
+abs_lib.sort(key=lambda x: x[0])
+
+for i in reversed(range(N)):
+    if abs_lib[i][0] == 0:
+        continue
+    complete, cnt = abs_lib[i][0], 1
+    for j in reversed(range(i)):
+        if cnt == M:
+            break
+        if abs_lib[j][0] != 0 and abs_lib[i][1] == abs_lib[j][1]:
+            abs_lib[j][0] = 0
+            cnt += 1
+    abs_lib[i][0] = 0
+    if i == N-1:
+        sum_step += complete
+    else:
+        sum_step += complete * 2
+print(sum_step)
