f11f974acb510f834cc5c525206dfb130589c911
e70e6916 <33220404+e70e6916@users.noreply.github.com>
2021-03-18 10:36:24 +0900

    Update BOJ_1715.py

--- BOJ/[1715] 카드 정렬하기/e70e6916/BOJ_1715.py
+++ BOJ/[1715] 카드 정렬하기/e70e6916/BOJ_1715.py
@@ -3,11 +3,10 @@ import heapq
 N = int(sys.stdin.readline())
 cards = list(int(sys.stdin.readline()) for _ in range(N))
 heapq.heapify(cards)
-card_sum = 0
+final_sum = 0
 
 while len(cards) > 1:
-    card_x = heapq.heappop(cards)
-    card_y = heapq.heappop(cards)
-    heapq.heappush(cards, card_x + card_y)
-    card_sum += card_x + card_y
-print(card_sum)
+    card_sum = heapq.heappop(cards) + heapq.heappop(cards)
+    heapq.heappush(cards, card_sum)
+    final_sum += card_sum
+print(final_sum)
