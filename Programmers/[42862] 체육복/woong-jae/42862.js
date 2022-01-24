40741194ef9e3309eb802ea63371adc90e13b7f7
Jae Woong Chung <33976823+woong-jae@users.noreply.github.com>
2022-01-24 17:57:54 +0900

    Update 42862.js

--- Programmers/[42862] 체육복/woong-jae/42862.js
+++ Programmers/[42862] 체육복/woong-jae/42862.js
@@ -1,97 +1,31 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
 function solution(n, lost, reserve) {
     let answer = 0;
-    let possibleStudent = new Array(n + 1).fill(true);
-
-    for(let lostIndex = 0; lostIndex < lost.length; lostIndex++) {
-        possibleStudent[lost[lostIndex]] = false; 
-    }
-
-    let reserveCheck = [];
-    for (let reserveIndex = 0; reserveIndex < reserve.length; reserveIndex++) {
-        if (!possibleStudent[reserve[reserveIndex]]) {
-            possibleStudent[reserve[reserveIndex]] = true;
-            continue;
+    
+    let students = new Array(n).fill(1);
+    
+    reserve.forEach(elem => students[elem - 1] = 2);
+    lost.forEach(elem => {
+        if(students[elem - 1] > 0) students[elem - 1]--;
+    });
+    
+    students.forEach((elem, i) => {
+        if(elem === 0) {
+            let left = i - 1, right = i + 1;
+            if(left >= 0 && students[left] > 1) {
+                students[left]--;
+                students[i]++;
+                answer++;
+            }
+            else if (right < n && students[right] > 1) {
+                students[right]--;
+                students[i]++;
+                answer++;
+            }
+        } 
+        else {
+            answer++;
         }
-        reserveCheck.push(reserve[reserveIndex]);
-    }
-
-    answer = simulateClothesLend(reserveCheck, 0, possibleStudent);
-
+    });
+    
     return answer;
-}
-
-function simulateClothesLend(reserve, reserveIndex, possibleStudent) {
-    let maxPossibleStudent = possibleStudent.reduce((prev, cur) => cur ? prev + 1 : prev) - 1;
-    if (reserveIndex >= reserve.length) return maxPossibleStudent;
-
-    const reserveStudent = reserve[reserveIndex];
-    if (checkBoundary(reserveStudent - 1, possibleStudent.length) && !possibleStudent[reserveStudent - 1]) {
-        possibleStudent[reserveStudent - 1] = true;
-        maxPossibleStudent = Math.max(maxPossibleStudent, simulateClothesLend(reserve, reserveIndex + 1, possibleStudent));   
-        possibleStudent[reserveStudent - 1] = false;
-    }
-
-    if (checkBoundary(reserveStudent + 1, possibleStudent.length) && !possibleStudent[reserveStudent + 1]) {
-        possibleStudent[reserveStudent + 1] = true;
-        maxPossibleStudent = Math.max(maxPossibleStudent, simulateClothesLend(reserve, reserveIndex + 1, possibleStudent));   
-        possibleStudent[reserveStudent + 1] = false;
-    }
-
-    maxPossibleStudent = Math.max(maxPossibleStudent, simulateClothesLend(reserve, reserveIndex + 1, possibleStudent));
-
-    return maxPossibleStudent;
-}
-
-function checkBoundary(target, bound) {
-    if (1 <= target && target < bound) return true;
-    return false;
 }
