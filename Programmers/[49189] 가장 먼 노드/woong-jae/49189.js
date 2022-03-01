d7292ef8e1883b198a0ba3438a7dbe540b75ca70
Jae Woong Chung <33976823+woong-jae@users.noreply.github.com>
2022-03-01 22:38:52 +0900

    Create 49189.js

---
+++ Programmers/[49189] 가장 먼 노드/woong-jae/49189.js
@@ -0,0 +1,37 @@
+function solution(n, edge) {
+    let answer = 0;
+    
+    const visited = new Array(n + 1).fill(false);
+    const adj_list = new Array(n + 1).fill(0).map(_ => []);
+    edge.forEach(e => {
+        adj_list[e[0]].push(e[1]);
+        adj_list[e[1]].push(e[0]);
+    });
+    
+    // BFS로 깊이 계산
+    let stack = [1];
+    let currentDepth = 1;
+    visited[1] = true;
+    while(1) {
+        let size = stack.length
+        let nextStack = [];
+        while(stack.length) {
+            let currentNode = stack.pop();
+            adj_list[currentNode].forEach(next => {
+               if(!visited[next]) {
+                   visited[next] = true;
+                   nextStack.push(next);
+               }
+            });
+        }
+        currentDepth++;
+        if(!nextStack.length) {
+            answer = size;
+            break;
+        }
+        stack = nextStack;
+    }
+    
+    return answer;
+}
+
